f1f3818f5470716b5048d92b35403dc48a31ceda
SMajerovic <shiframajerovic@gmail.com>
2024-05-28 07:07:04 -0400

    session 20 spec

---
+++ 0 spec.sql
@@ -0,0 +1,62 @@
+/* 
+I own a cemetery called Whisper Woods. Whisper Woods was established in January of 1713.
+We are transitioning from paper records to an electronic record keeper. This will make it easier to direct visitors 
+to who they would like to visit. It will also be easier for us to keep track of which plots are taken and what kind of grave care people paid for. 
+
+
+We need to keep track of everyone that is buried in the cemetery. We need to record their first name, last name, and middle name if applicable. 
+We need to have their date of birth and date of death. We also want to record the date they purchased the plot and how much they purchased it for.
+We will also need to record what section, street name, and spot number they are buried in. 
+
+The cemetery has a grounds upkeep team that maintains the appearance of the graves but not everyone pays for this service. We need to record who 
+purchased a perpetual care package so that we can make sure to stay on top of it. 
+
+Each record has a unique identification code. We use the first letter of their last name, the first two letters of their first name, and then the spot number. 
+
+There are three sections in the cemetery, Section A, B and C. It is then made up of road names and on each road we have a spot number. 
+
+Not always are plots pre-purchased, the purchase of a plot will often only be happen the person has passed away.
+
+Questions: 
+    Q: Will you be inputting records of those that purchased a burial plot in advance of death date?
+    A: Yes, we also record once a plot is purchased so that we can ensure a space remains open for them. 
+
+    Q: Are the spot numbers repetitive?
+    A: No, each spot in the cemetary has a unique number. We do not reuse the same number. However, it is split up by section. 
+        In section A we have numbers 1 - 20,000
+        In section B we have numbers 20,000 - 40,000
+        In section C we have numbers 40,000 - 60,000
+
+    Q: Aside from the perpetual care package, is there any other maintenance package that can be purchased? 
+    A: No, we like to keep our grounds neat and clean so we do general upkeep. The perpetual care package ensures extra attention and care is given to those graves. 
+
+    Q: Do you offer double-depth burials, meaning, would you have more than one body in a grave?
+    A: No, our personal belief is that it is a disrespectful practice. There will never be more than one person in one grave. 
+
+
+I would like to produuce the following reports:
+    1) I need a list that will be useful to the maintenance crew. I would like to see a list of last name, first name, the section, spot number 
+    and if they purchased perpetual care or not.
+
+    2) We have 20,000 plots available in each section. It is helpful for us to know how many plots have not been filled. 
+    I would like to know how many people are buried in each section of the cemetary.
+
+    3) For those that passed away, how many years it has been since the person passed away
+
+    4) I would like to see the ages of those that passed away
+
+
+Sample Date:
+    Last Name, First Name, Middle Initial, Date of Birth, Date of Death, Date of Purchase, Price of Purchase, Section, Road Name, Spot Number, Perpetual Care
+    Morris, Alan, T, 07/15/1795, 02/27/1875, 01/04/1860, $1500, C, Evergreen Drive, 40001, Yes
+    Shapiro, Fanny, 01/01/1997, 08/22/2017, 08/23/2017, $3600, A, Maple Lane, 116, Yes
+    Lerman, Renee, 11/16/1889, 09/01/1968, 06/25/1946, $2200, C, Fern Way, 50125, No
+    Roman, Jonah, L, 03/22/1974, 05/30/2022, 03/10/1999, $3000, B, Cherry Drive, 20000, Yes
+    Kurman, Ava, C, 02/16/2003, 10/12/2023, 10/12/2023, $4200, A, Oak Street, 11, Yes
+    Combs, Bertha, 04/14/1674, 03/12/1713, 03/14/1713, $500, B, Cherry Drive, 20003, No
+    Oriel, Craig, N 02/08/1943, 12/30/2001, 06/08/1999, $3000, C, Elm Street, 40579, No
+    Brown, Rachel, 05/26/2000, 01/22/2024, 01/20/2024, $4200, A, Maple Lane, 136, Yes
+    Fine, Charles, W, 09/06/1880, 09/23/1974, 05/04/1920, $2200, C, Evergreen Drive, 40023, Yes
+    Smith, Alana, L, 08/20/1921, 02/15/1985, 02/16/1985, $3000, B, Cedar Lane, 30145, Yes
+
+*/
